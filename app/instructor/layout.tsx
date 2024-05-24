1d1694c6d29d3938720d39000fbb74712fa53c56
hassanuahmad <ahmad12p@uwindsor.ca>
2024-05-24 01:25:55 -0400

    added instrcutor navbar
    added the navbar but have to edit and make sure some parts of it are server side because right now all of it is client side. when i remove the use client, it shows an error! Have to fix that

---
+++ app/instructor/layout.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next";
+import InstructorNavbar from "@/components/instructorNavbar";
+
+export const metadata: Metadata = {
+  title: "Instructor Layout",
+};
+
+export default function InstructorLayout({
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section>
+      <InstructorNavbar />
+      {children}
+    </section>
+  );
+}
