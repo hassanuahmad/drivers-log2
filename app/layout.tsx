16f4b3fd606867352b7375d482e9c142fd01291e
hassanuahmad <ahmad12p@uwindsor.ca>
2024-06-23 20:45:55 -0400

    added speed insights for vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 import { Toaster } from "sonner";
@@ -24,6 +25,7 @@ export default function RootLayout({
         <main>{children}</main>
         <Toaster position="top-right" richColors={true} />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
