01f66909a1c2b8f8d18aaef07c948542a441c187
hassanuahmad <ahmad12p@uwindsor.ca>
2024-08-17 22:47:09 -0400

    made the export button mobile responsive

--- app/instructor/vehicle/view.tsx
+++ app/instructor/vehicle/view.tsx
@@ -149,7 +149,7 @@ export default function View({
                     }
                 >
                     <File className="h-3.5 w-3.5" />
-                    <span className="sr-only sm:not-sr-only">Export CSV</span>
+                    <span>Export CSV</span>
                 </Button>
             </div>
             {/* Export Button End */}
